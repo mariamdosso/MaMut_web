5bba24ba9fc32efd3aee2df0633d19c4efdfa510
mariamdosso <dosomarie02@gmail.com>
2025-03-06 14:27:07 +0000

    resume 2 dashboard

--- controller/home_controller.php
+++ controller/home_controller.php
@@ -2,13 +2,12 @@
     include("config/db.php");
 
     $queryMembers = $pdo->query("SELECT COUNT(*) as total FROM member");
-    $members = $queryMembers->fetch(PDO::FETCH_ASSOC); // Utilisation de fetch() au lieu de fetchAll()
-
+    $members = $queryMembers->fetch(PDO::FETCH_ASSOC); 
     $queryevent = $pdo->query("SELECT COUNT(*) as total FROM event");
-    $events = $queryevent->fetch(PDO::FETCH_ASSOC); // Utilisation de fetch() au lieu de fetchAll()
+    $events = $queryevent->fetch(PDO::FETCH_ASSOC); 
 
     $querycaisse = $pdo->query("SELECT COUNT(*) as total FROM fund");
-    $fund = $querycaisse->fetch(PDO::FETCH_ASSOC); // Utilisation de fetch() au lieu de fetchAll()
+    $fund = $querycaisse->fetch(PDO::FETCH_ASSOC); 
 ?> 
 
 
